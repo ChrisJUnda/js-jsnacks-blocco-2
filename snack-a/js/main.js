3d981c2cdce708d6e2678257561a37bd552d80fb
ChrisJUnda <christian.segoviaunda@gmail.com>
2024-05-12 10:42:24 +0200

    [UPD] Snack-A finished

---
+++ snack-a/js/main.js
@@ -0,0 +1,18 @@
+'use strict'
+
+
+
+//input
+const input = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+
+//output
+console.log(`la lunghezza dell'array è ${contaElementi(input)} elementi`);
+
+//function
+function contaElementi(input) {
+
+    return input.length;
+
+    console.log(input)
+}
